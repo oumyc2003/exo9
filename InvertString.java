3de9e174925fc99a8fd2074ae5908f6fc4dd6977
Votre Nom <oumycisse1610@gmail.com>
2024-01-17 00:57:33 +0000

    prooo

---
+++ InvertString.java
@@ -0,0 +1,16 @@
+public class InvertString {
+    public static void main(String[] args) {
+        // Chaîne de caractères à inversers
+        String nom = "Bonjour";
+
+        // Création d'un objet StringBuilder avec la chaîne originale
+        StringBuilder stringBuilder = new StringBuilder(nom);
+
+        // Inversion de la chaîne à l'aide de la méthode reverse() de StringBuilder
+        stringBuilder.reverse();
+
+        // Affichage de la chaîne inversée
+        System.out.println("Chaîne originale : " + nom);
+        System.out.println("Chaîne inversée  : " + stringBuilder .String();
+    }
+}
